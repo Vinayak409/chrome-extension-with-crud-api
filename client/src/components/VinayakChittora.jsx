2e0a322141f7fbaa0fb8e32bde8fa894c6afc9c9
Vinayak Chittora <vinayakchittora@gmail.com>
2023-11-04 21:49:11 +0530

    files added

---
+++ client/src/components/VinayakChittora.jsx
@@ -0,0 +1,18 @@
+
+import {styled, Card, Typography} from '@mui/material';
+
+const StyledDiv = styled('div')`
+  & > p{
+    font-size: 25px;
+  }
+`
+
+const VinayakChittora = () => {
+  return (
+    <Card>
+      <Typography>This is simple crud app built using mern stack</Typography>
+    </Card>
+  )
+}
+
+export default VinayakChittora
